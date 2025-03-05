c9e32355399a403ccbb573af39233594c189b1c3
MunIL HONG <bear4564@naver.com>
2025-03-05 17:52:02 +0900

    category

---
+++ board-back/src/main/java/com/korit/boardback/dto/response/RespBoardListSearchDto.java
@@ -0,0 +1,24 @@
+package com.korit.boardback.dto.response;
+
+
+import com.korit.boardback.entity.BoardSearch;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RespBoardListSearchDto {
+    private int page;
+    private int limitCount;
+    private int totalPages;
+    private int totalElements;
+    private boolean isFirstPage;
+    private boolean isLastPage;
+    private List<BoardSearch> boardSearchList;
+}
--- board-back/src/main/java/com/korit/boardback/service/BoardService.java
+++ board-back/src/main/java/com/korit/boardback/service/BoardService.java
@@ -1,10 +1,8 @@
 package com.korit.boardback.service;
 
+import com.korit.boardback.dto.request.ReqBoardListSearchDto;
 import com.korit.boardback.dto.request.ReqWriteBoardDto;
-import com.korit.boardback.entity.Board;
-import com.korit.boardback.entity.BoardCategory;
-import com.korit.boardback.entity.BoardCategoryAndBoardCount;
-import com.korit.boardback.entity.User;
+import com.korit.boardback.entity.*;
 import com.korit.boardback.repository.BoardCategoryRepository;
 import com.korit.boardback.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,33 +13,50 @@ import java.util.List;
 
 @Service
 public class BoardService {
-    @Autowired
-    private BoardRepository boardRepository;
+
     @Autowired
     private BoardCategoryRepository boardCategoryRepository;
+    @Autowired
+    private BoardRepository boardRepository;
 
     @Transactional(rollbackFor = Exception.class)
     public Board createBoard(String categoryName, User user, ReqWriteBoardDto reqWriteBoardDto) {
         BoardCategory boardCategory = boardCategoryRepository
                 .findByName(categoryName)
-                .orElseGet(()-> {
+                .orElseGet(() -> {
                     BoardCategory bc = BoardCategory.builder()
                             .boardCategoryName(categoryName)
                             .build();
-                   return boardCategoryRepository.save(bc);
+                    return boardCategoryRepository.save(bc);
                 });
+
         Board board = Board.builder()
                 .boardCategoryId(boardCategory.getBoardCategoryId())
                 .userId(user.getUserId())
                 .title(reqWriteBoardDto.getTitle())
                 .content(reqWriteBoardDto.getContent())
                 .build();
-
         return boardRepository.save(board);
     }
 
-
-    public List<BoardCategoryAndBoardCount> getBoardCategoriesByBoardId(User user) {
+    public List<BoardCategoryAndBoardCount> getBoardCategoriesByUserId(User user) {
         return boardCategoryRepository.findAllByUserId(user.getUserId());
     }
+
+    @Transactional(readOnly = true) //읽기전용 최적화
+    public List<BoardSearch> getBoardListSearchBySearchOption(ReqBoardListSearchDto reqBoardListSearchDto) {
+        int startIndex = (reqBoardListSearchDto.getPage() - 1) * reqBoardListSearchDto.getLimitCount();
+        return boardRepository.findBoardListAllBySearchOption(
+                startIndex,
+                reqBoardListSearchDto.getLimitCount(),
+                reqBoardListSearchDto.getOrder(),
+                reqBoardListSearchDto.getSearchText()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public int getBoardListCountBySearchText(String searchText) {
+        return boardRepository.findBoardCountAllBySearchText(searchText);
+    }
+
 }
